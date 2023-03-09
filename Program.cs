a842816390584a31c816c11620bb4d27508d9f1a
Surendra Chaluvadi <surendra.chaluvadi@gmail.com>
2023-03-09 23:55:38 +0400

    read from appsettings.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
 ﻿using Azure.Identity;
 using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
 
-var scopes = new[] { "User.Read" };
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+var config = builder.Build();
+var azureId = config.GetSection("AzureAd").Get<AzureAd>();
+var downStreamApi = config.GetSection("DownstreamApi").Get<DownstreamApi>();
+
+var scopes = downStreamApi.Scopes.Split(' ');
 var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
 {
-    ClientId = "811cecb2-1cef-45f3-845d-942914cee31e"
+    ClientId = azureId.ClientId,
+    TenantId = azureId.TenantId,
+    AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+    RedirectUri = new Uri(azureId.RedirectUri)
 };
+
 var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
 
 var graphClient = new GraphServiceClient(tokenCredential, scopes);
 
 var me = await graphClient.Me.GetAsync();
 Console.WriteLine($"Hello {me?.DisplayName}!");
+
+public record AzureAd
+(
+    string Instance,
+    string Domain,
+    string TenantId,
+    string ClientId,
+    string CallbackPath,
+    string Scopes,
+    string RedirectUri
+);
+
+public record DownstreamApi
+(
+    string BaseUrl,
+    string Scopes
+);
