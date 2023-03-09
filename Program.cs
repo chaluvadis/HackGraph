37ef42e7b89611ae396bda606249cf4c6eceb154
Surendra Chaluvadi <surendra.chaluvadi@gmail.com>
2023-03-09 22:46:09 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+
+var scopes = new[] { "User.Read" };
+var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+{
+    ClientId = "811cecb2-1cef-45f3-845d-942914cee31e"
+};
+var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+
+var graphClient = new GraphServiceClient(tokenCredential, scopes);
+
+var me = await graphClient.Me.GetAsync();
+Console.WriteLine($"Hello {me?.DisplayName}!");
